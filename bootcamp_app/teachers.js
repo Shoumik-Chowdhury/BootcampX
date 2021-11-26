8050c71c24420a797531a8c2a1330887d8faa60d
Shoumik-Chowdhury <shoumik.atique@gmail.com>
2021-11-26 02:14:14 +0000

    update to use parameterized queries

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -12,8 +12,8 @@ FROM teachers
 JOIN assistance_requests ON teacher_id = teachers.id
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name = '${process.argv[2]}'
-ORDER BY teacher;`)
+WHERE cohorts.name = $1
+ORDER BY teacher;`, [`${process.argv[2]}`])
 .then(res => {
   res.rows.forEach(element => {
     console.log(`${element.cohort}: ${element.teacher}`);
